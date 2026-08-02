78e39204d930868a082686879fccd751e0ae61d7
linai2026 <dan@Dans-MacBook-Pro.local>
2026-08-03 04:35:23 +0900

    Week02 Day01

---
+++ week02/python/overflow_demo.py
@@ -0,0 +1,10 @@
+import math
+import torch
+
+print(math.exp(10))
+print(math.exp(100))
+print(math.exp(500))
+
+print(torch.exp(torch.tensor(10.)))
+print(torch.exp(torch.tensor(100.)))
+print(torch.exp(torch.tensor(500.)))
